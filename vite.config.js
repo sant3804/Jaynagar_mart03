29c19fa5ee702830245ece5e4db2028ee9955f36
sant3804 <sk1231384@gmail.com>
2025-10-28 20:18:12 +0530

    Configure for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/jaynagar_mart03/',  // For GitHub Pages deployment
+  base: '/',  // For Vercel deployment
 })
